9dcc2a9a4e67e145e2a1277e97f130bddbc32473
Honglin(John) Wei <41243908+johnwei417@users.noreply.github.com>
2020-08-12 21:56:35 -0400

    Add files via upload

---
+++ src/Compare.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+//used for priority queue;
+public class Compare implements Comparator<HufTree>{
+
+    @Override
+    public int compare(HufTree o1, HufTree o2) {
+        if(o1.weight < o2.weight)
+            return -1;
+        else if(o1.weight > o2.weight)
+            return 1;
+        return 0;
+    }
+
+}
